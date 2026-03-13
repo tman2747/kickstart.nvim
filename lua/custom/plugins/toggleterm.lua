18d8cc050fc07393b82d383093206bb745649119
Triston Land <tristonland@gmail.com>
2026-03-12 19:51:00 -0600

    change terminal command to normal mode only

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,7 +2,13 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    open_mapping = '|',
     direction = 'horizontal', -- or 'horizontal' or 'vertical 'float''
   },
+  config = function()
+    require('toggleterm').setup {
+      direction = 'horizontal',
+    }
+    -- set up the keymap for normal mode only
+    vim.keymap.set('n', '|', '<cmd>ToggleTerm<cr> ', { desc = 'Toggle Terminal' })
+  end,
 }
